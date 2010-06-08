51272f8f4e18d64badaf1b0f6e886298949944cb
Salmaan Rashid <salrashid123@gmail.com>
2010-06-08 18:29:32 +0000

    Added cursor support to testutility  (each cursor now in a separate file)

--- projects/salesforce/src/com/google/enterprise/connector/salesforce/TestUtility.java
+++ projects/salesforce/src/com/google/enterprise/connector/salesforce/TestUtility.java
@@ -14,8 +14,13 @@
 
 package com.google.enterprise.connector.salesforce;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Properties;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
@@ -29,6 +34,7 @@ import javax.xml.transform.stream.StreamSource;
 
 import org.w3c.dom.Document;
 
+import com.google.enterprise.connector.salesforce.exception.BaseException;
 import com.google.enterprise.connector.salesforce.modules.salesforce.SFQuery;
 import com.google.enterprise.connector.salesforce.modules.salesforce.SalesForceLogin;
 
@@ -64,16 +70,16 @@ public class TestUtility {
 						ConsoleHandler console = new ConsoleHandler();
 						FileHandler filelogger = new FileHandler("TestUtility.log");
 						
-						//set the default logger to the highest (this is a test utility afterall)
-						logger.setLevel(Level.FINEST);
-						filelogger.setFormatter(new SimpleFormatter());					
-						logger.addHandler(console);
+						//set the default logger to the INFO (this is a test utility afterall)
+						logger.setLevel(Level.INFO);
+						console.setFormatter(new SimpleFormatter());
+						filelogger.setFormatter(new SimpleFormatter());		
+						console.setLevel(Level.FINE);
+						//logger.addHandler(console);
 						logger.addHandler(filelogger);
 						
-
-						
 						if (args.length<1){
-							logger.log(Level.INFO,"Usage:  com.google.enterprise.connector.salesforce.TestUtility (optional)<configfile.txt> <queryXSLT>");
+							logger.log(Level.INFO,"Usage:  com.google.enterprise.connector.salesforce.TestUtility <init.properties>  (optional)<queryXSLT>");
 							logger.log(Level.INFO,"where configfile.txt is: ");
 							logger.log(Level.INFO,"   username=: ");
 							logger.log(Level.INFO,"   password=: ");
@@ -85,7 +91,8 @@ public class TestUtility {
 						//info about a goog base64 converter (no line breaks!)
 						logger.log(Level.FINE,"Base64 Encocer: http://base64-encoder-online.waraxe.us/");
 
-
+						int cursor_counter = 1;
+						
 						Properties props = new Properties();
 						props.load(new FileInputStream(args[0]));
 						
@@ -94,46 +101,116 @@ public class TestUtility {
 						String loginserver = props.getProperty("loginserver");
 						String query = props.getProperty("query");
 
+						boolean do_transform = false;
+						
+						if (args.length>1)
+							do_transform = true;
+						
+					    Calendar cal = Calendar.getInstance();
+					    SimpleDateFormat sdf = new SimpleDateFormat("hh_mm_ss");
+					    String strDirectory = sdf.format(cal.getTime());
+					    
+					    boolean success = (new File(strDirectory)).mkdir();
+					    if (success) {
+					    	logger.log(Level.INFO,("Directory: " + strDirectory + " created"));
+					    }
+					    else {
+					    	logger.log(Level.SEVERE,("Unable to create Directory: " + strDirectory + " ...exiting"));
+					    	System.exit(0);
+					    }
+						
 						//do a salesforce login given the props
 						SalesForceLogin sfl = new SalesForceLogin(username,password,loginserver);
 						
 						if (sfl.isLoggedIn()){
 							//if logged in, run a query
-							//The following query just runs one time without checking for a cursor
-							//TODO: implement a cursor (pretty easy to do)
+
 							SFQuery sfq = new SFQuery();
 							sfq.submitStatement(query, BaseConstants.QUERY_TYPE, sfl.getEndPointServer(), sfl.getSessionID());
 							Document soap_response_doc = sfq.submitStatement(query, BaseConstants.QUERY_TYPE, sfl.getEndPointServer(), sfl.getSessionID());
 							String xml_response = Util.XMLDoctoString(soap_response_doc);
 							
-							logger.log(Level.INFO, "Query Response XML " + xml_response);
-							
-							//if we need to xslt transform the response.
-							if (args.length>1){
-								TransformerFactory tFactory = TransformerFactory.newInstance();
-								Transformer transformer = tFactory.newTransformer(new StreamSource(new FileInputStream(args[1])));
-								 
-								    StringBuilder xsltcontents = new StringBuilder();
-								    BufferedReader input =  new BufferedReader(new FileReader(args[1]));
-	
-								    String line = null; 
-								        while (( line = input.readLine()) != null){
-								        	xsltcontents.append(line);
-								        	xsltcontents.append(System.getProperty("line.separator"));
-								        }
-								        
-								 Document XSLTDoc = Util.XMLStringtoDoc(xsltcontents.toString());
-								 							 
-								 Document doc_transformed = Util.TransformDoctoDoc(soap_response_doc, XSLTDoc);
-	
-								 logger.log(Level.INFO,"Transformed Response " + Util.XMLDoctoString(doc_transformed));
+					    	//do we have a cursor in the response
+						    String q_locator = sfq.getQueryLocator(soap_response_doc);
+						    						    						    						    
+						    String filename = strDirectory + System.getProperty("file.separator") + "response_" + cursor_counter  + ".xml";						    
+						    logger.log(Level.INFO, "Writing response to " + filename);
+						    FileWriter fstream = new FileWriter(filename);
+					        BufferedWriter out = new BufferedWriter(fstream);
+					        out.write(xml_response);
+					        out.close();
+					    							
+							if (do_transform) {
+							    String tx_resp = this.TransformDoc(args[1],soap_response_doc);
+							    filename = strDirectory + System.getProperty("file.separator") + "tx_" + cursor_counter + ".xml";						    
+							    logger.log(Level.INFO, "Writing transform to " + filename);
+							    fstream = new FileWriter(filename);
+						        out = new BufferedWriter(fstream);
+						        out.write(tx_resp);
+						        out.close();
 							}
-							 
+							
+						    //if we do, then query again
+						    while (q_locator != null) {
+							    logger.log(Level.FINE, "Query locator cursor found " + q_locator);
+							    cursor_counter++;
+							     soap_response_doc =sfq.submitStatement(q_locator, BaseConstants.QUERYMORE_TYPE,sfl.getEndPointServer(), sfl.getSessionID()) ;							     
+								 if (soap_response_doc==null)
+									   	throw new BaseException("QUERY RESPONSE document is null");
+								xml_response = Util.XMLDoctoString(soap_response_doc);	
+								logger.log(Level.INFO, "Query Response XML " + xml_response);
+								
+							    filename = strDirectory + System.getProperty("file.separator") + "response_" + cursor_counter  + ".xml";						    
+							    logger.log(Level.INFO, "Writing response to " + filename);
+							    fstream = new FileWriter(filename);
+						        out = new BufferedWriter(fstream);
+						        out.write(xml_response);
+						        out.close();
+								
+							    q_locator = sfq.getQueryLocator(soap_response_doc);	
+								if (do_transform) {
+								    String tx_resp = this.TransformDoc(args[1],soap_response_doc);
+								    filename = strDirectory + System.getProperty("file.separator") + "tx_" + cursor_counter + ".xml";						    
+								    logger.log(Level.INFO, "Writing transform to " + filename);
+								    fstream = new FileWriter(filename);
+							        out = new BufferedWriter(fstream);
+							        out.write(tx_resp);
+							        out.close();
+								}
+
+						    } 
 						}
 						
 			        }
 			        catch (Exception ex) {
 			        	logger.log(Level.SEVERE, "Error running tests: " + ex);
 			        }					      
+	}
+	
+	private String TransformDoc(String xsltfile,Document soap_response_doc){
+		
+		try{
+			TransformerFactory tFactory = TransformerFactory.newInstance();
+			Transformer transformer = tFactory.newTransformer(new StreamSource(new FileInputStream(xsltfile)));
+			 
+			    StringBuilder xsltcontents = new StringBuilder();
+			    BufferedReader input =  new BufferedReader(new FileReader(xsltfile));
+
+			    String line = null; 
+			        while (( line = input.readLine()) != null){
+			        	xsltcontents.append(line);
+			        	xsltcontents.append(System.getProperty("line.separator"));
+			        }
+			        
+			 Document XSLTDoc = Util.XMLStringtoDoc(xsltcontents.toString());		 							 
+			 Document doc_transformed = Util.TransformDoctoDoc(soap_response_doc, XSLTDoc);
+			 
+			 return Util.XMLDoctoString(doc_transformed);
+			 //logger.log(Level.INFO,"Transformed Response " + Util.XMLDoctoString(doc_transformed));
+		}
+	catch (Exception ex){
+		logger.log(Level.SEVERE, "Error Transforming tests: " + ex);
+		return null;
+	}
 	}
 }
